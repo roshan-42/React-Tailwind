ca68a5179aae20613285aee36bb6582044532a65
Roshan Acharya <roshanacharya207@gmail.com>
2024-01-16 12:18:39 +0545

    error

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,33 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
-const menuItems = [
-  {
-    title: "Home",
-    key: "/",
-  },
-  {
-    title: "Projects",
-    key: "/projects",
-  },
-  {
-    title: "Courses",
-    key: "/courses",
-  },
-  {
-    title: "Contact",
-    key: "/contact",
-  },
-];
-
 function Header() {
+  const [showMenu, setShowMenu] = useState("md:hidden");
+
+  const menuItems = [
+    {
+      title: "Home",
+      key: "/",
+    },
+    {
+      title: "Projects",
+      key: "/projects",
+    },
+    {
+      title: "Courses",
+      key: "/courses",
+    },
+    {
+      title: "Contact",
+      key: "/contact",
+    },
+  ];
   return (
     <div className="text-white font-mont">
       <div className="flex bg-theme justify-between items-center p-2 shadow-lg">
-        <div>
+        <div className="flex justify-between w-full">
           <h1 className="text-4xl font-semibold">ROSHAN</h1>
+          <button
+            onClick={() => {
+              setShowMenu();
+            }}
+            className="md:flex lg:hidden xl:hidden 2xl:hidden"
+          >
+            Menu
+          </button>
         </div>
-        <div className="flex">
+        <div className="flex md:hidden">
+          {menuItems.map((item, index) => {
+            return (
+              <li className="list-none mx-5">
+                <Link to={item.key}>{item.title}</Link>
+              </li>
+            );
+          })}
+        </div>
+        <div className="md:flex flex-col lg:hidden 2xl:hidden xl:hidden">
           {menuItems.map((item, index) => {
             return (
               <li className="list-none mx-5">
